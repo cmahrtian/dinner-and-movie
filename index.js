98237efa60c0f79086c4bb751cd9b80e468b7b35
Chris Mahr <mahrtian@gmail.com>
2016-02-29 13:22:44 -0500

    save after adding very basic front-end and back-end js functionality

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var api = new Showtimes(10001, {});
  
 // console.log(api.getTheaters);
 
+// "user strict";
+
+// $(.'pure-button').click(function() {
+//   event.preventDefault();
+//   alert("Current input is" + $('.pure-input').val());
+// });
+
 api.getTheaters(function (error, theaters) {
   if (error) {
     throw error
